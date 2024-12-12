63dce1d685f871af5b18307ba6b2c414a2ab5cd6
Silas Morales <silas.mora22@gmail.com>
2024-12-11 23:41:50 -0800

    completed hamburger and close icon animation

--- src/components/MobileMenuModal.tsx
+++ src/components/MobileMenuModal.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { RefObject, useEffect, useRef, useState } from 'react'
 import AuthButtons from './AuthButtons'
 import Navbar from './Navbar'
 
+type isModalOpenProps = {
+  isModalOpen?: boolean
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>> // Function to update state
+}
 
-const MobileMenuModal = () => {
+const MobileMenuModal: React.FC<isModalOpenProps> = ({isModalOpen, setIsModalOpen}) => {
+
+  const [isModalFadeIn, setIsModalFadeIn] = useState<boolean>(false)
+  const modalRef = useRef<HTMLDivElement>(null)
+
+  const handleClickOutside = (e: React.MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      setIsModalOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    if (isModalOpen) {
+      setIsModalFadeIn(true)
+    } else {
+      setIsModalFadeIn(false)
+    }
+  })
+
   return (
-    <div>
-      <div className='bg-darkByzantineBlue mx-6 px-6 rounded-[10px] h-[383px] flex flex-col items-center justify-center gap-7'>
+    <div onMouseDown={handleClickOutside} className={`ease-in-out duration-500 ${isModalFadeIn? 'translate-x-0': 'translate-x-full'}`}>
+      <div ref={modalRef} className='bg-darkByzantineBlue mx-6 px-6 rounded-[10px] h-[383px] flex flex-col items-center justify-center gap-7'>
         <div>
           <Navbar isModal={true}/>
         </div>
